c8bd31c5dcc8e3c42e31ebc7c08547b904341443
TheSicilian12 <mguidera314@gmail.com>
2024-01-05 13:26:12 -0500

    timer function clearInterval fixed

--- timer.js
+++ timer.js
@@ -3,11 +3,14 @@ function timer(holiday) {
     let holidayObj = {
         'christmas': [new Date("Dec 25, " + todayYear + " 00:00:00").getTime(), 'Christmas'],
         'newYearsEve': [new Date("Dec 31, " + todayYear + " 00:00:00").getTime(), "New Year's Eve"],
+        'now': [new Date(), 'now'],
     }
 
     let now = Date.now();
     let holidayDate = holidayObj[holiday][0]
     let timeDif = holidayDate - now;
+
+    if (timeDif <= 0) return -1
 
     let days = Math.floor(timeDif / (1000 * 60 * 60 * 24))
     let hours = Math.floor(timeDif % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
