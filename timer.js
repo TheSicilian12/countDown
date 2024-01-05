176f2a1d31f43b0653119a33faa4d1b8febd8ba8
TheSicilian12 <mguidera314@gmail.com>
2024-01-05 12:34:24 -0500

    set up timer js

--- timer.js
+++ timer.js
@@ -1,21 +1,19 @@
-function timer() {
-    // let now = Date.now(holidayName);
-    // let timeDif = timerDate - now;
+function timer(holiday) {
+    let todayYear = new Date().getFullYear();
+    let holidayObj = {
+        'christmas': [new Date("Dec 25, " + todayYear + " 00:00:00").getTime(), 'Christmas'],
+        'newYearsEve': [new Date("Dec 31, " + todayYear + " 00:00:00").getTime(), "New Year's Eve"],
+    }
 
-    // let days = Math.floor(timeDif / (1000 * 60 * 60 * 24))
-    // let hours = Math.floor(timeDif % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-    // let minutes = Math.floor(timeDif % (1000 * 60 * 60) / (1000 * 60));
-    // let seconds = Math.floor(timeDif % (1000 * 60) / 1000);
+    let now = Date.now();
+    let holidayDate = holidayObj[holiday][0]
+    let timeDif = holidayDate - now;
 
-    // let todayYear = new Date().getFullYear();
+    let days = Math.floor(timeDif / (1000 * 60 * 60 * 24))
+    let hours = Math.floor(timeDif % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor(timeDif % (1000 * 60 * 60) / (1000 * 60));
+    let seconds = Math.floor(timeDif % (1000 * 60) / 1000);
 
-    // let holidayObj = {
-    //     'christmas': [new Date("Dec 25, " + todayYear + " 00:00:00").getTime(), 'Christmas'],
-    //     'newYearsEve': [new Date("Dec 31, " + todayYear + " 00:00:00").getTime(), "New Year's Eve"],
-    // }
-
-    // // let holidayName = localStorage.getItem('holidayName');
-    // let timerDate = holidayObj[holidayName][0];
-
-    return 'hello!'
+    let str = days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds ';
+    return str
 };
